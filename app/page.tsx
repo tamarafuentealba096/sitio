c9d7ad674c785106b6d159b158d61385b5d9244c
tamarafuentealba096 <tamarafuentealba096@gmail.com>
2025-02-10 21:52:06 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import { Scheduler, Instagram } from "@/components/features";
-import { EventType } from "@/types/event";
-import { query } from "./service";
-
-export const revalidate = 60;
 
 export default async function Home() {
-  const events: EventType[] = await query();
-
   return (
     <main>
-      <Scheduler events={events} />
+      <Scheduler events={[]} />
       <Instagram />
     </main>
   );
